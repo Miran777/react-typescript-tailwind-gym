e2149b69f2d68b20427ca2d6803b1fe7441bee44
Mirzekhan Mirzekhanov <121393083+Miran777@users.noreply.github.com>
2023-02-07 21:16:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Navbar from "@/scenes/navbar"
-import Home from "@/scenes/home"
-import Benefits from "@/scenes/benefits"
-import OurClasses from "@/scenes/ourClasses"
-import ContactUs from "@/scenes/ContactUs"
-import Footer from "@/scenes/Footer"
+import Navbar from "./scenes/navbar"
+import Home from "./scenes/home"
+import Benefits from "./scenes/benefits"
+import OurClasses from "./scenes/ourClasses"
+import ContactUs from "./scenes/contactUs"
+import Footer from "./scenes/footer"
 import { useEffect, useState } from 'react'
 import { SelectedPage } from "./shared/types"
 
